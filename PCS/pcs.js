ca21c56c762f7061e1b92bad2b8adbb9dc7c1f43
vedant027 <vedant.thorat27@gmail.com>
2022-10-30 16:51:35 +0530

    pcs folder added

--- PCS/pcs.js
+++ PCS/pcs.js
@@ -1,107 +1,91 @@
+// ============================== NAVBAR ==================================
+
 // change navbar styles on scroll
 
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle
+  document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
-  })
-
-
-
-
-const main_video = document.querySelector('.main-video video');
-const main_video_title = document.querySelector('.main-video .title');
-const video_playlist = document.querySelector('.video-playlist .videos');
-
-let data = [
-    {
-        'id': 'a1',
-        'title': 'manipulate text background',
-        'name': 'manipulate text background.mp4',
-        'duration': '2:47',
-    },
-    {
-        'id': 'a2',
-        'title': 'build gauge with css',
-        'name': 'build gauge with css.mp4',
-        'duration': '2:45',
-    },
-    {
-        'id': 'a3',
-        'title': '3D popup card',
-        'name': '3D popup card.mp4',
-        'duration': '24:49',
-    },
-
-    {
-        'id': 'a4',
-        'title': 'customize HTML5 form elements',
-        'name': 'customize HTML5 form elements.mp4',
-        'duration': '3:59',
-    },
-    {
-        'id': 'a5',
-        'title': 'custom select box',
-        'name': 'custom select box.mp4',
-        'duration': '4:25',
-    },
-    {
-        'id': 'a6',
-        'title': 'embed google map to contact form',
-        'name': 'embed google map to contact form.mp4',
-        'duration': '5:33',
-    },
-    {
-        'id': 'a7',
-        'title': 'password strength checker javascript web app',
-        'name': 'password strength checker javascript web app.mp4',
-        'duration': '0:29',
-    },
-
-    {
-        'id': 'a8',
-        'title': 'custom range slider',
-        'name': 'custom range slider.mp4',
-        'duration': '1:12',
-    },
-    {
-        'id': 'a9',
-        'title': 'animated shopping cart',
-        'name': 'animated shopping cart.mp4',
-        'duration': '3:38',
-    },
-
-];
-
-data.forEach((video, i) => {
-    let video_element = `
-                <div class="video" data-id="${video.id}">
-                    <img src="images/play.svg" alt="">
-                    <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
-                    <h3 class="title">${video.title}</h3>
-                    <p class="time">${video.duration}</p>
-                </div>
-    `;
-    video_playlist.innerHTML += video_element;
 })
 
-let videos = document.querySelectorAll('.video');
-videos[0].classList.add('active');
-videos[0].querySelector('img').src = 'images/pause.svg';
 
-videos.forEach(selected_video => {
-    selected_video.onclick = () => {
+// ============================== DROPDOWN =================================
+const videoPlayer = document.querySelector("iframe");
 
-        for (all_videos of videos) {
-            all_videos.classList.remove('active');
-            all_videos.querySelector('img').src = 'images/play.svg';
+const videoTitle = document.querySelector('.video-title');
 
-        }
+const list = document.querySelector('.module-list');
+// const videoTitle = document.queryselector('.videoTitle');
+console.log('data------:', list);
 
-        selected_video.classList.add('active');
-        selected_video.querySelector('img').src = 'images/pause.svg';
+list.addEventListener('click', (data) => {
+  console.log('data------:', data.target);
+  const videoId = data.target.getAttribute('data-id');
+  console.log('data------:', videoId);
 
-        let match_video = data.find(video => video.id == selected_video.dataset.id);
-        main_video.src = 'videos/' + match_video.name;
-        main_video_title.innerHTML = match_video.title;
-    }
+  const title = data.target.getAttribute('data-title'); 
+  let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+  videoPlayer.setAttribute("src", videoUrl);
+
+  videoTitle.innerHTML = title;
 });
+
+// const dropdowns = document.querySelectorAll('.dropdown');
+// //Loop through all dropdown elements
+// dropdowns.forEach(dropdown => {
+//   //Get inner elements from each dropdown
+//   const select = dropdown.querySelector('.select');
+//   const caret = dropdown.querySelector('.caret');
+//   const menu = dropdown.querySelector('.menu');
+//   const options = dropdown.querySelectorAll('.menu li');
+//   const selected = dropdown.querySelectorAll('.selected');
+
+
+
+//   //Add a click event to the select element
+//   select.addEventListener('click', () => {
+//     //Add the clicked select styles to the select element
+//     select.classList.toggle('select-clicked');
+//     //Add the rotate styles to the caret element
+//     caret.classList.toggle('caret-rotate');
+//     //Add the open styles to the menu element
+//     menu.classList.toggle('menu-open');
+//   });
+
+
+//   //Loop through all option elements
+//   options.forEach(option => {
+//     //Add a click event to the option element
+//     option.addEventListener('click', () => {
+//       //Change selected inner text to clicked option inner text
+//       selected.innerText = option.innerText;
+//       select.classList.remove('select-clicked');
+//       //Add the rotate styles to the caret element
+//       caret.classList.remove('caret-rotate');
+//       //Add the open styles to the menu element
+//       menu.classList.remove('menu-open');
+//       //Remove active class from all option elements
+//       options.forEach(option => {
+//         option.classList.remove('active');
+//       });
+//       //Add active class to clicked option element
+//       option.classList.add('active');
+//     });
+//   });
+// });
+
+
+// ================================== Video Section ================================ 
+
+
+// const showVideo = (videoId, title) => {
+//   let videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+//   videoPlayer.setAttribute("src", videoUrl);
+//   videoTitle.innerHTML = title;
+//   showVideo(v.dataset.title);
+// };
+
+// dropdownMenu.forEach((v) => {
+//   v.addEventListener("click", () => {
+//     showVideo(v.dataset.id);
+//   });
+// });
